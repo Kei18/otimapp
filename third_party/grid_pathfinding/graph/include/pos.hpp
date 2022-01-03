414731814f974b09fc5142e12e311200b3f19b90
Kei18 <keisuke.oku18@gmail.com>
2022-01-03 18:39:27 +0900

    add anonymous submission

---
+++ third_party/grid_pathfinding/graph/include/pos.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+struct Pos {
+  int x;
+  int y;
+
+  Pos(int _x, int _y);
+  ~Pos();
+
+  void print() const;
+  void println() const;
+  int manhattanDist(const Pos& pos) const;
+  float euclideanDist(const Pos& pos) const;
+  bool operator==(const Pos& other) const;
+  Pos operator+(const Pos& other) const;
+  Pos operator-(const Pos& other) const;
+  Pos operator*(const int i) const;
+  void operator+=(const Pos& other);
+  void operator-=(const Pos& other);
+  void operator*=(const int i);
+};
---
+++ third_party/grid_pathfinding/readme.md
@@ -0,0 +1,12 @@
+Grid Pathfinding
+===
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+A simple graph library with single-agent pathfinding algorithms.
+It is written in C++(17) with [CMake](https://cmake.org/) (≥v3.16) build.
+For usage, see `./tests`.
+
+map format: [Pathfinding Benchmarks](https://movingai.com/benchmarks/)
+
+## Licence
+This software is released under the MIT License, see [LICENSE.txt](LICENCE.txt).
